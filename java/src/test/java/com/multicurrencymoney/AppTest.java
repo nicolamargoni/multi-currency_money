60d72bd612cf76346a4db9b771a0f5de2edeeb82
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-03 17:43:42 +0100

    created test currency

--- java/src/test/java/com/multicurrencymoney/AppTest.java
+++ java/src/test/java/com/multicurrencymoney/AppTest.java
@@ -34,4 +34,10 @@ public class AppTest {
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
 }
