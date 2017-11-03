65a3684b98c2ffe9d52f1536941330b49c6a0697
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-03 12:31:42 +0100

    created first test: testMultiplication

---
+++ java/src/test/java/com/multicurrencymoney/AppTest.java
@@ -0,0 +1,15 @@
+package test.java.com.multicurrencymoney;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AppTest {
+
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10,five.amount);
+    }
+}
