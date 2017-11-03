23dd61f0b97431e4830681a9dea57df3df3bdf9d
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-03 16:45:00 +0100

    moved equals method to Money

--- java/src/main/java/com/multicurrencymoney/Money.java
+++ java/src/main/java/com/multicurrencymoney/Money.java
@@ -2,4 +2,9 @@ package main.java.com.multicurrencymoney;
 
 public class Money {
     protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
