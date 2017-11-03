bfd9b0f03411ad009007d995cffd74a533a90268
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-03 16:50:39 +0100

    upgrade testEqualty

--- java/src/test/java/com/multicurrencymoney/AppTest.java
+++ java/src/test/java/com/multicurrencymoney/AppTest.java
@@ -28,6 +28,8 @@ public class AppTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 
 }
