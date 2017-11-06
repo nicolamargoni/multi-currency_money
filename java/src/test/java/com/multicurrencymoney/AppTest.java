c0ca3b2133c4e4881c03a88b5648541bd363bc33
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-06 11:32:05 +0100

    created simple addition test

--- java/src/test/java/com/multicurrencymoney/AppTest.java
+++ java/src/test/java/com/multicurrencymoney/AppTest.java
@@ -43,4 +43,10 @@ public class AppTest {
         assertTrue(new Money(10, "CHF").equals(new Money(10, "CHF")));
     }
 
+
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
