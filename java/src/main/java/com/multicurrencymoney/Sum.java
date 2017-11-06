3d8b74d45141fda607f44965283286eea80a33c0
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-06 12:27:02 +0100

    implemented Sum class

--- java/src/main/java/com/multicurrencymoney/Sum.java
+++ java/src/main/java/com/multicurrencymoney/Sum.java
@@ -1,6 +1,11 @@
 package main.java.com.multicurrencymoney;
 
-public class Sum {
+public class Sum implements Expression {
     public Money augend;
     public Money addend;
+
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
 }
